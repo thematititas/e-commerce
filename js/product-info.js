1555162248fa4f04a1b9ce0c38852bc87576358b
thematititas <88540297+thematititas@users.noreply.github.com>
2021-09-13 00:55:24 -0300

    Nuevos jsons

--- js/product-info.js
+++ js/product-info.js
@@ -1,32 +1,50 @@
 var producto;
-var comentario;
+var comentarios;
 
 
-function showProduct(product, comments) {
+function showImages(array) {
 
-    let content = "";
-    let img = "";
+    let contenido = "";
 
-    content += `<a href="product-info.html" class="list-group-item list-group-item-action" onclick="ID(` + product.id + `)">
-<div class="row">
-    <div class="col-3">
-        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-    </div>
-    <div class="col">
-        <div class="d-flex w-100 justify-content-between">
-            <h4 class="mb-1">`+ product.name + `</h4>
-            <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+    for (let i = 0; i < array.length; i++) {
+        let images = array[i];
+
+        contenido += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
+            </div>
         </div>
-        <p class="mb-1">` + product.description + `</p> 
-        <br>
-        <h5 class="mb-1">` + product.currency + "  " + product.cost + `</h5>
-    </div>  
-</div>
-</a>
-`;
-    document.getElementById("cat-list-container").innerHTML = content;
+        `
 
+        document.getElementById("Imagen").innerHTML = contenido;
+    }
+}
 
+function showStars(numero) {
+    let Stars = "";
+    for (let i = 0; i < numero; i++) {
+        Stars += '<span class="fa fa-star checked"></span>'
+    }
+    for (let i = 0; i < (5 - numero); i++) {
+        Stars += '<span class="fa fa-star"></span>'
+    }
+    return Stars;
+}
+
+function showComentario(coment) {
+
+    let comentario = "";
+
+    comentario += `
+        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">`+ coment.user + `:</h4>
+                        <small class="text-muted">` + coment.dateTime + `</small>
+                    </div>
+                    <p >` + coment.description + `</p>`
+    comentario += showStars(coment.score) + `<hr>`;
+
+    document.getElementById("comentarios").innerHTML += comentario;
 }
 
 
@@ -35,7 +53,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (result) {
         if (result.status === "ok") {
-            comentario = result.data;
+            result.data.forEach(Coment =>{
+                if (coment.id == JSON.parse(localStorage.getItem('id')).productId){
+                    comentarios = coment;
+                }
+            });
         }
 
         getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
@@ -43,7 +65,24 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 resultObj.data.forEach(product => {
                     if (product.id == JSON.parse(localStorage.getItem('id')).productId) {
                         producto = product;
-                        showProduct(producto, comentario);
+
+                        let nameHTML = product.name;
+                        let descriptionHTML = product.description;
+                        let soldCountHTML = product.soldCount;
+                        let currencyHTML = product.currency;
+                        let costHTML = product.cost;
+
+                        document.getElementById("name").innerHTML = nameHTML;
+                        document.getElementById("Description").innerHTML = descriptionHTML;
+                        document.getElementById("Count").innerHTML = soldCountHTML;
+                        document.getElementById("Cost").innerHTML = currencyHTML;
+                        document.getElementById("Cost").innerHTML += " " + costHTML;
+
+                        showImages(producto.images);
+                        comentarios.forEach(comentario => {
+                            showComentario(comentario);
+                        });
+
                     }
                 });
             }
